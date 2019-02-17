1ad2f56090ec2ccc63b350c969d566f007e33621
Hamza Al-Amin <hizhiz70@gmail.com>
2019-02-17 18:23:04 +0000

    Implemented player death for hot potato, and organised folders.

---
+++ 206RealWorld/Assets/Scripts/Hot Potato/PotatoTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotatoTimer : MonoBehaviour
+{
+  public float explosionCountdown;
+  public Text countText;
+    // Start is called before the first frame update
+    void Start()
+    {
+      explosionCountdown = 15.50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      explosionCountdown -= Time.deltaTime;
+      countText.text =  explosionCountdown.ToString("f0");
+    }
+}
