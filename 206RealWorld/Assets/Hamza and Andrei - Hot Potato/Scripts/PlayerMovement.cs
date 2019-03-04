b1b4a8aa9717c9800b69bf38e9c15da68e5a04b6
ConraD22 <andrei.dumitran@gmail.com>
2019-03-04 13:59:54 +0000

    aaa

--- 206RealWorld/Assets/Hamza and Andrei - Hot Potato/Scripts/PlayerMovement.cs
+++ 206RealWorld/Assets/Hamza and Andrei - Hot Potato/Scripts/PlayerMovement.cs
@@ -8,7 +8,6 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 0.1f;
     public int playerNum;
     public bool hot;
-    int players;
 
     public KeyCode upward;
     public KeyCode downward;
@@ -20,13 +19,15 @@ public class PlayerMovement : MonoBehaviour
     SpriteRenderer playerspriteproperties;
     RoundStart startingHot;
     PotatoTimer playerDeath;
-    public ScoreSystem score;
 
     public void Start()
     {
         playerspriteproperties = GetComponent<SpriteRenderer>();
         playerDeath = GameObject.Find("countdownText").GetComponent<PotatoTimer>();
         startingHot = GameObject.Find("RoundStarter").GetComponent<RoundStart>();
+
+
+        WhoIsHot();
     }
 
     void Update()
@@ -34,12 +35,7 @@ public class PlayerMovement : MonoBehaviour
         Move();
 
         if (hot == true){
-            playerspriteproperties.color = Color.red;
-            Die();
-        }
-        else
-        {
-            playerspriteproperties.color = Color.white;
+            BecomeHot();
         }
 
     }
@@ -52,43 +48,88 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(rightward) == true) transform.Translate(speed, 0f, 0f);
     }
 
-    private void Die()
+    private void BecomeHot()
     {
+        playerspriteproperties.color = Color.red;
         if (playerDeath.explosionCountdown < 1)
         {
-            if(startingHot.hotStarter == 1)
-            {
-                if (startingHot.playerList.Count == 4) score.player1Score += 1;
-                else if (startingHot.playerList.Count == 3) score.player1Score += 2;
-                else if (startingHot.playerList.Count == 2) score.player1Score += 3;
-                else if (startingHot.playerList.Count == 1) score.player1Score += 5;
-            }
-            else if (startingHot.hotStarter == 2)
-            {
-                 if (startingHot.playerList.Count == 4) score.player2Score += 1;
-                else if (startingHot.playerList.Count == 3) score.player2Score += 2;
-                else if(startingHot.playerList.Count == 2) score.player2Score += 3;
-                else if (startingHot.playerList.Count == 1) score.player2Score += 5;
-            }
-            else if (startingHot.hotStarter == 3)
-            {
-                 if(startingHot.playerList.Count == 4) score.player3Score += 1;
-                else if(startingHot.playerList.Count == 3) score.player3Score += 2;
-                else if(startingHot.playerList.Count == 2) score.player3Score += 3;
-                else if(startingHot.playerList.Count == 1) score.player3Score += 5;
-            }
-            else if (startingHot.hotStarter == 4)
-            {
-                 if(startingHot.playerList.Count == 4) score.player4Score += 1;
-                else if(startingHot.playerList.Count == 3) score.player4Score += 2;
-                else if(startingHot.playerList.Count == 2) score.player4Score += 3;
-                else if(startingHot.playerList.Count == 1) score.player4Score += 5;
-            }
-
-            Debug.Log("Player " + startingHot.hotStarter.ToString() + " has been eliminated!   and " + startingHot.playerList.Count.ToString() + " left");
+            Debug.Log("Player " + startingHot.hotStarter.ToString() + " has been eliminated!");
             Destroy(GameObject.Find("Player " + startingHot.hotStarter.ToString()));
             startingHot.playerList.Remove(startingHot.hotStarter);
+
             
+        }
+    }
+
+    private void WhoIsHot()
+    {
+        if (playerNum == 1 && startingHot.hotStarter == 1)
+        {
+            player1.hot = true;
+            player2.hot = false;
+            player3.hot = false;
+            player4.hot = false;
+            Debug.Log("Player 1 starts with the Hot Potato.");
+        }
+        if (playerNum == 2 && startingHot.hotStarter == 2)
+        {
+            player1.hot = false;
+            player2.hot = true;
+            player3.hot = false;
+            player4.hot = false;
+            Debug.Log("Player 2 starts with the Hot Potato.");
+        }
+        if (playerNum == 3 && startingHot.hotStarter == 3)
+        {
+            player1.hot = false;
+            player2.hot = false;
+            player3.hot = true;
+            player4.hot = false;
+            Debug.Log("Player 3 starts with the Hot Potato.");
+        }
+        if (playerNum == 4 && startingHot.hotStarter == 4)
+        {
+            player1.hot = false;
+            player2.hot = false;
+            player3.hot = false;
+            player4.hot = true;
+            Debug.Log("Player 4 starts with the Hot Potato.");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player1" && !player1.hot)
+        {
+            startingHot.hotStarter = 1;
+            player1.hot = true;
+            player2.hot = false;
+            player3.hot = false;
+            player4.hot = false;
+        }
+        if (collision.gameObject.tag == "Player2" && !player2.hot)
+        {
+            startingHot.hotStarter = 2;
+            player1.hot = false;
+            player2.hot = true;
+            player3.hot = false;
+            player4.hot = false;
+        }
+        if (collision.gameObject.tag == "Player3" && !player3.hot)
+        {
+            startingHot.hotStarter = 3;
+            player1.hot = false;
+            player2.hot = false;
+            player3.hot = true;
+            player4.hot = false;
+        }
+        if (collision.gameObject.tag == "Player4" && !player4.hot)
+        {
+            startingHot.hotStarter = 4;
+            player1.hot = false;
+            player2.hot = false;
+            player3.hot = false;
+            player4.hot = true;
         }
     }
 }
--- 206RealWorld/Assets/Hamza and Andrei - Hot Potato/Scripts/PotatoTimer.cs
+++ 206RealWorld/Assets/Hamza and Andrei - Hot Potato/Scripts/PotatoTimer.cs
@@ -10,7 +10,7 @@ public class PotatoTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      explosionCountdown = 5.50f;
+      explosionCountdown = 15.50f;
     }
 
     // Update is called once per frame
