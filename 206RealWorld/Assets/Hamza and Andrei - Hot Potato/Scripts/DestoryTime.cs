85f867c7e80be962192f7f820d5b856267cabf68
ConraD22 <andrei.dumitran@gmail.com>
2019-03-11 00:01:40 +0000

    Reupload of Week 6
    Reupload of week 6

---
+++ 206RealWorld/Assets/Hamza and Andrei - Hot Potato/Scripts/DestoryTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryTime : MonoBehaviour
+{
+
+    public float destroyTime;
+
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+}
