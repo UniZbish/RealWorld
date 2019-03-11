deb333e69fffbf76cac4b6e4ff5aa64d6fb128b7
RealWorldProject <wingslifer@yahoo.co.uk>
2019-03-11 04:17:27 +0000

    Prefab and Lobby
    Created prefab for players points and fixed alobby audio issue

---
+++ 206RealWorld/Assets/Scripts/Lobby/Scoremanager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoremanager : MonoBehaviour
+{
+    public int firstPlacePoints = 30, secondPlacePoints = 25, thirdPlacePoints = 15, fourthPlacePoints = 5;
+    public int firstPlace, secondPlace, thirdPlace, fourthPlace;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.transform);
+    }
+}
